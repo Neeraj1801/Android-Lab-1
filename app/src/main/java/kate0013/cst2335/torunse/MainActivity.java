6e39b06ceaa32422b63add95f5cc69b04e241ed7
Neeraj1801 <neerajkatewa2@gmail.com>
2023-02-07 18:32:45 -0500

    Finished Phone Version of Lab 3

--- app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
+++ app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
@@ -1,52 +1,34 @@
 package kate0013.cst2335.torunse;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import kate0013.cst2335.torunse.data.MainActivityViewModel;
-import kate0013.cst2335.torunse.databinding.ActivityMainBinding;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-private ActivityMainBinding binding;
-    MainActivityViewModel viewModel;
+    ImageView imgView;
+    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.button.setOnClickListener(click->{
-            viewModel.editTextContents.postValue(binding.editText.getText().toString());
-        });
-        viewModel.isSelected.observe(this, selected-> {
-            binding.myCheckBox.setChecked(selected);
-            binding.myRadioButton.setChecked(selected);
-            binding.mySwitch.setChecked(selected);
-            binding.textView.setText("The value is: "+binding.editText.getText().toString());
-            Context context= getApplicationContext();
-            CharSequence text="Your value is: "+binding.myCheckBox.isChecked();
-            int duration=Toast.LENGTH_LONG;
-            Toast.makeText(context,text,duration).show();
-        });
-        binding.myCheckBox.setOnCheckedChangeListener((myCheckBox,isChecked)->{
-            viewModel.isSelected.postValue(isChecked);
-        });
-        binding.myRadioButton.setOnCheckedChangeListener((myRadioButton,isChecked)->{
-            viewModel.isSelected.postValue(isChecked);
-        });
-        binding.mySwitch.setOnCheckedChangeListener((mySwitch,isChecked)->{
-            viewModel.isSelected.postValue(isChecked);
-        });
-        binding.myImage.setOnClickListener(click->{
-            Toast.makeText(MainActivity.this,"Width: "+binding.myImage.getWidth()+" Length: "+binding.myImage.getHeight(),Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+        imgView = findViewById(R.id.imageView);
+        sw = findViewById(R.id.switch1);
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
         });
     }
 }
--- app/src/main/java/kate0013/cst2335/torunse/data/MainActivityViewModel.java
+++
@@ -1,9 +0,0 @@
-package kate0013.cst2335.torunse.data;
-
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
-
-public class MainActivityViewModel extends ViewModel {
-    public MutableLiveData<String> editTextContents=new MutableLiveData<>();
-    public MutableLiveData<Boolean> isSelected=new MutableLiveData<>();
-}
