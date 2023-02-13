b31016de59689499f557359b412b8dc19f1b487d
Neeraj1801 <neerajkatewa2@gmail.com>
2023-02-13 18:42:01 -0500

    Finished TV Version of Lab 3

--- app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
+++ app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
@@ -1,34 +1,68 @@
 package kate0013.cst2335.torunse;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Switch;
 
+import kate0013.cst2335.torunse.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    ImageView imgView;
-    Switch sw;
+    private static String Tag="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityMainBinding binding=ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        imgView = findViewById(R.id.imageView);
-        sw = findViewById(R.id.switch1);
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
+        setContentView(binding.getRoot());
+        SharedPreferences prefs=getSharedPreferences("MyData",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor= prefs.edit();
+        editor.putString("Email",binding.editText.getText().toString());
+        editor.apply();
+        binding.button.setOnClickListener(btn->{
+            Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+            String typedEmail=binding.editText.getText().toString();
+            String typedPassword=binding.editText2.getText().toString();
+            intent.putExtra("Email",typedEmail);
+            intent.putExtra("Password",typedPassword);
+            String found=prefs.getString("Email","Missing Data");
+            startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( Tag, "Now Visible" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( Tag, "Now listens for touch" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( Tag, "On Pause" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( Tag, "Now stopped" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/kate0013/cst2335/torunse/SecondActivity.java
@@ -0,0 +1,90 @@
+package kate0013.cst2335.torunse;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import kate0013.cst2335.torunse.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySecondBinding binding= ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        File file = new File( getFilesDir(), "Picture.png");
+        if(file.exists())
+        {
+            Bitmap theImage= BitmapFactory.decodeFile(getFilesDir().getPath()+"/Picture.png");
+            binding.imageView.setImageBitmap(theImage);
+        }
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+
+                    @Override
+
+                    public void onActivityResult(ActivityResult result) {
+
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+
+                            Intent data = result.getData();
+                            Bitmap thumbnail=data.getParcelableExtra("data");
+                            FileOutputStream fOut = null;
+
+
+                            try { fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                                thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                                fOut.flush();
+                                fOut.close();
+                            }
+                            catch (FileNotFoundException e)
+                            {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+
+                });
+        SharedPreferences prefs=getSharedPreferences("MyPhone",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor= prefs.edit();
+        editor.apply();
+        Intent fromPrevious=getIntent();
+        String emailAddress=fromPrevious.getStringExtra("Email");
+        binding.textView3.setText("Welcome back "+ emailAddress);
+        binding.secondPageButton.setOnClickListener( click-> {
+            String phone=binding.editTextPhone.getText().toString();
+            editor.putString("PhoneNumber",binding.editTextPhone.getText().toString());
+            editor.apply();
+            Intent dial=new Intent(Intent.ACTION_DIAL);
+            dial.setData(Uri.parse("tel:"+phone));
+            startActivity(dial);
+        });
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        binding.button3.setOnClickListener(click->{
+            cameraResult.launch(cameraIntent);
+        });
+    }
+}
