afaebf4153eccaff69eb20f9672aea8c8d873189
Neeraj1801 <neerajkatewa2@gmail.com>
2023-03-06 18:03:17 -0500

    Added JavaDoc comments and finished 5 test cases.

--- app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
+++ app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
@@ -1,52 +1,103 @@
 package kate0013.cst2335.torunse;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
+import static kate0013.cst2335.torunse.R.id.loginButton;
+import static kate0013.cst2335.torunse.R.id.passWordField;
+import static kate0013.cst2335.torunse.R.id.passWordTextView;
 
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import kate0013.cst2335.torunse.data.MainActivityViewModel;
-import kate0013.cst2335.torunse.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-private ActivityMainBinding binding;
-    MainActivityViewModel viewModel;
+
+    /**
+     * variable of type TextView to store the id of TextView
+     */
+    private TextView textView = null;
+
+    /**
+     * variable of type EditText to store the id of editText
+     */
+    private EditText editText = null;
+
+    /**
+     * variable of type Button to hold the id of the button
+     */
+    private Button loginButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.button.setOnClickListener(click->{
-            viewModel.editTextContents.postValue(binding.editText.getText().toString());
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.passWordTextView);
+        editText = findViewById(R.id.passWordField);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(click -> {
+            String userValue = editText.getText().toString();
+            if (checkPasswordComplexity(userValue))
+                textView.setText("Your password is complex enough");
+            else
+                textView.setText("You shall not pass!");
         });
-        viewModel.isSelected.observe(this, selected-> {
-            binding.myCheckBox.setChecked(selected);
-            binding.myRadioButton.setChecked(selected);
-            binding.mySwitch.setChecked(selected);
-            binding.textView.setText("The value is: "+binding.editText.getText().toString());
-            Context context= getApplicationContext();
-            CharSequence text="Your value is: "+binding.myCheckBox.isChecked();
-            int duration=Toast.LENGTH_LONG;
-            Toast.makeText(context,text,duration).show();
-        });
-        binding.myCheckBox.setOnCheckedChangeListener((myCheckBox,isChecked)->{
-            viewModel.isSelected.postValue(isChecked);
-        });
-        binding.myRadioButton.setOnCheckedChangeListener((myRadioButton,isChecked)->{
-            viewModel.isSelected.postValue(isChecked);
-        });
-        binding.mySwitch.setOnCheckedChangeListener((mySwitch,isChecked)->{
-            viewModel.isSelected.postValue(isChecked);
-        });
-        binding.myImage.setOnClickListener(click->{
-            Toast.makeText(MainActivity.this,"Width: "+binding.myImage.getWidth()+" Length: "+binding.myImage.getHeight(),Toast.LENGTH_LONG).show();
-        });
+    }
+
+    /**
+     * function with if and else cases to check if entered password follows the criteria
+     *
+     * @param password is the entered value of type string
+     * @return returns a boolean value
+     */
+    private boolean checkPasswordComplexity(String password) {
+        boolean isDigitState, isUpperCaseState, isLowerCaseState, foundSpecialState;
+        isDigitState = isUpperCaseState = isLowerCaseState = foundSpecialState = false;
+        //Iterating through the entered password
+                for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isUpperCase(c))
+                isUpperCaseState = true;
+            if (Character.isLowerCase(c))
+                isLowerCaseState = true;
+            if (Character.isDigit(c))
+                isDigitState = true;
+
+            switch (c) {
+                case '#':
+                case '$':
+                case '%':
+                case '^':
+                case '&':
+                case '*':
+                case '!':
+                case '@':
+                case '?':
+                    foundSpecialState = true;
+                    break;
+                default:
+                    foundSpecialState = false;
+                    break;
+            }
+        }
+        // checking for various password requirements if they exist in the entered password of not
+        if (!isUpperCaseState) {
+            Toast.makeText(this, "Password requires Uppercase!!!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!isLowerCaseState) {
+            Toast.makeText(this, "Password requires Lowercase!!!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!isDigitState) {
+            Toast.makeText(this, "Password requires digit!!!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundSpecialState) {
+            Toast.makeText(this, "Password requires special character!!!", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            Toast.makeText(this, "You have a strong password!!!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
     }
 }
