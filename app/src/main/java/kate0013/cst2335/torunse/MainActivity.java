2ea91809c3e86e6c663c00ced7156590c9facded
Neeraj1801 <neerajkatewa2@gmail.com>
2023-03-07 16:58:05 -0500

    Final Commit

--- app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
+++ app/src/main/java/kate0013/cst2335/torunse/MainActivity.java
@@ -5,10 +5,6 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import kate0013.cst2335.torunse.data.MainActivityViewModel;
---
+++ app/src/main/java/kate0013/cst2335/torunse/data/ChatMessage.java
@@ -0,0 +1,29 @@
+package kate0013.cst2335.torunse.data;
+
+
+public class ChatMessage {
+    private String message;
+    private String timeSent;
+    private boolean isSendButton;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String message, String timeSent, boolean isSendButton) {
+        this.message = message;
+        this.timeSent = timeSent;
+        this.isSendButton = isSendButton;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public boolean getIsSendButton() {
+        return isSendButton;
+    }
+}
